a52d3a54e1b23ba2f2631e031c6501e949076ad6
olavmulder <olavmulder02@gmail.com>
2023-07-21 12:53:29 +0200

    add fucntion

--- Core/Src/uart_communication.c
+++ Core/Src/uart_communication.c
@@ -8,17 +8,39 @@
 
 #include "uart_communication.h"
 
+<<<<<<< HEAD
 volatile bool receviedUART = false;
 
 void UART_Task()
 
 {
+=======
+volatile bool receivedUART = false;
+uint32_t message_id = 0;
+void UART_Task()
+
+{
+
+>>>>>>> 3b48119 (init)
 	while(1)
 	{
 		if(receivedUART)
 		{
 			//handle UART
 			receivedUART = false;
+<<<<<<< HEAD
 		}
 	}
 }
+=======
+			message_id++;
+
+		}
+	}
+}
+
+void UART_Send()
+{
+
+}
+>>>>>>> 3b48119 (init)
